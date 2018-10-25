e24c064b781a638354fecd9bdb5bb79645fc5113
AI-JiangBBOC <40155137+AI-JiangBBOC@users.noreply.github.com>
2018-10-25 20:06:31 +0800

    Add files via upload

---
+++ JiChuDaiMa/TestIf.java
@@ -0,0 +1,11 @@
+package mypro01;
+/*
+ *测试if语句
+ */
+public class TestIf {
+	public static void main(String[] args) {
+		double d = Math.random();
+		int e = (int)(d * 5);//得到一个0到4的随机整数。
+		System.out.println(e);
+	}
+}
